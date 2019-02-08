024e34624d786052f27f608ffa0f0b97e6e12966
abstract-animation-9914 <vm930@hotmail.com>
2019-02-08 19:25:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -15,18 +15,3 @@ def current_player(board)
     puts "O"
   end 
 end 
-
-def turn_count(board)
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-  end
-  turns
-
-end
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
